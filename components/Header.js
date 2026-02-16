00b6e55f45af829c61720da775429e30d72d7358
LuidJ-78 <emile.peillon@yahoo.com>
2026-02-16 08:26:56 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -12,56 +12,22 @@ export default function Header() {
       <a href="#" onClick={() => router.push(baseUrl)} style={{ color: '#007BFF', fontWeight: 'bold' }}> Accueil </a>
       <a href="https://fshub.io/airline/AZH/radar" target="_blank" rel="noopener noreferrer" style={{ color: '#007BFF', fontWeight: 'bold' }}> Radar </a>
       <div className="dropdown">
-        <a
-          href="#"
-          onClick={() => router.push(`${baseUrl}planificateur/`)}
-          style={{ color: '#007BFF', fontWeight: 'bold' }}
-        >
-          Planificateur de vol
-        </a>
+        <a href="#" onClick={() => router.push(`${baseUrl}planificateur/`)} style={{ color: '#007BFF', fontWeight: 'bold' }}> Planificateur de vol </a>
         <div className="dropdown-content">
-          <a
-            href="#"
-            onClick={() => router.push(`${baseUrl}planificateur/ou_voler/`)}
-            style={{ color: '#007BFF', fontWeight: 'bold' }}
-          >
-            Ou voler ?
-          </a>
+          <a href="#" onClick={() => router.push(`${baseUrl}planificateur/ou_voler/`)} style={{ color: '#007BFF', fontWeight: 'bold' }}> Ou voler ? </a>
         </div>
       </div>
       <div className="dropdown">
-        <a href="#" style={{ color: '#007BFF', fontWeight: 'bold' }}>
-          Carte de voyage
-        </a>
+        <a href="#" style={{ color: '#007BFF', fontWeight: 'bold' }}> Carte de voyage </a>
         <div className="dropdown-content">
-          <a
-            href="#"
-            onClick={() => router.push(`${baseUrl}passager/`)}
-            style={{ color: '#007BFF', fontWeight: 'bold' }}
-          >
-            Passager
-          </a>
-          <a
-            href="#"
-            onClick={() => router.push(`${baseUrl}cargo/`)}
-            style={{ color: '#007BFF', fontWeight: 'bold' }}
-          >
-            Cargo
-          </a>
+          <a href="#" onClick={() => router.push(`${baseUrl}passager/`)} style={{ color: '#007BFF', fontWeight: 'bold' }}> Passager </a>
+          <a href="#" onClick={() => router.push(`${baseUrl}cargo/`)} style={{ color: '#007BFF', fontWeight: 'bold' }}>  Cargo </a>
         </div>
       </div>
       <div className="dropdown">
-        <a href="#" style={{ color: '#007BFF', fontWeight: 'bold' }}>
-          Base de donnée
-        </a>
+        <a href="#" style={{ color: '#007BFF', fontWeight: 'bold' }}> Base de donnée </a>
         <div className="dropdown-content">
-          <a
-            href="#"
-            onClick={() => router.push(`${baseUrl}database/avion/`)}
-            style={{ color: '#007BFF', fontWeight: 'bold' }}
-          >
-            Avion
-          </a>
+          <a href="#" onClick={() => router.push(`${baseUrl}database/avion/`)} style={{ color: '#007BFF', fontWeight: 'bold' }}> Avion </a>
         </div>
       </div>
     </div>
